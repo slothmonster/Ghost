bcd690c30c585057c1d2bd44757edfcf84e5d49a
Adam Deibert <ardeibert@gmail.com>
2014-04-26 01:03:14 -0700

    added email env vars

--- config.js
+++ config.js
@@ -49,7 +49,16 @@ config = {
     // Configure your URL and mail settings here
     production: {
         url: 'http://ardeibert.com',
-        mail: {},
+        mail: {
+            transport: 'SMTP',
+            options: {
+                service: 'Gmail',
+                auth: {
+                    user: process.env.EMAIL_USER,
+                    pass: process.env.EMAIL_PASS
+                }
+            }
+        },
         database: {
             client: 'sqlite3',
             connection: {
